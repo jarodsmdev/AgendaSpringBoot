b70a44239b8b2bcfaff977c96b7cc2f5a803fe2a
Jarod Smith <lbriones.dev@gmail.com>
2023-05-23 22:14:01 -0400

    11. Actualizar contacto con thymeleaf

--- src/main/java/com/jarodSmith/agenda/controller/ContactoController.java
+++ src/main/java/com/jarodSmith/agenda/controller/ContactoController.java
@@ -7,10 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 @Controller
@@ -29,7 +31,7 @@ public class ContactoController {
     @GetMapping("/nuevo")
     ModelAndView nuevo(){
 
-        return new ModelAndView("nuevo")
+        return new ModelAndView("form")
                 .addObject("contacto", new Contacto());
     }
 
@@ -37,7 +39,7 @@ public class ContactoController {
     ModelAndView crear(@Validated Contacto contacto, BindingResult bindingResult, RedirectAttributes ra){
 
         if(bindingResult.hasErrors()){
-            return new ModelAndView("nuevo")
+            return new ModelAndView("form")
                     .addObject("contacto", contacto);
         }
         contactoRepository.save(contacto);
@@ -46,4 +48,36 @@ public class ContactoController {
         return new ModelAndView("redirect:/");
     }
 
+    @GetMapping("/{id}/editar")
+    ModelAndView editar(@PathVariable Integer id){
+
+        Contacto contacto = contactoRepository
+                .findById(id)
+                .orElseThrow(EntityNotFoundException::new);
+
+        return new ModelAndView("form")
+                .addObject("contacto", contacto);
+    }
+
+    @PostMapping("/{id}/editar")
+    ModelAndView actualizar(@PathVariable Integer id,
+                            @Validated Contacto contacto,
+                            BindingResult bindingResult,
+                            RedirectAttributes redirectAttributes){
+
+        contactoRepository
+                .findById(id)
+                .orElseThrow(EntityNotFoundException::new);
+
+        if(bindingResult.hasErrors()){
+            return new ModelAndView("form")
+                    .addObject("contacto", contacto);
+        }
+
+        contacto.setId(id);
+        contactoRepository.save(contacto);
+
+        redirectAttributes.addFlashAttribute("msgExito", "¡El contacto fue actualizado correctamente!");
+        return new ModelAndView("redirect:/");
+    }
 }
